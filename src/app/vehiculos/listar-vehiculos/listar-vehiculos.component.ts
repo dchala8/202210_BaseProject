4698f3b0d913a6338d936a6d8bb2941a67187979
dchala8 <d.chala@uniandes.edu.co>
2022-04-30 18:43:44 -0500

    Se crea la vista y pruebas solicitadas

--- src/app/vehiculos/listar-vehiculos/listar-vehiculos.component.ts
+++ src/app/vehiculos/listar-vehiculos/listar-vehiculos.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Vehiculo } from '../vehiculo';
+import { VehiculosService } from '../vehiculos.service'
 
 @Component({
   selector: 'app-listar-vehiculos',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarVehiculosComponent implements OnInit {
 
-  constructor() { }
+  constructor(private vehiculosService: VehiculosService) { }
+
+
+  listaVehiculos:Vehiculo[]= [];
+  listaMarcas: String[]= [];
+  listaInstances: Number[]= [];
 
   ngOnInit(): void {
+    this.vehiculosService.getVehiculos().subscribe((response:Vehiculo[]) =>{
+      this.listaVehiculos = response;
+      this.listaVehiculos.forEach((vehiculo:Vehiculo) => {
+        let found =this.listaMarcas.find((element:String) => element == vehiculo.marca)
+        if(found){
+
+        }else{
+          this.listaMarcas.push(vehiculo.marca);
+          this.listaInstances.push(this.getTotalMarca(vehiculo.marca));
+        }
+      })
+    })
+  }
+
+  getTotalMarca(marca:String){
+    let listaApariciones = this.listaVehiculos.filter((vehicleToFind:Vehiculo) => vehicleToFind.marca == marca)
+    return listaApariciones.length
   }
 
 }
